3bfbd86ca2128851ec37a02035aa03ea7a6f084a
MrWhiteHawk <gavwang@qq.com>
2022-06-12 17:34:50 +1000

    first complete app

---
+++ bibledrawiolib.js
@@ -0,0 +1,326 @@
+import sqlt3 from 'better-sqlite3'
+import bb from "bluebird"
+import pppt from 'puppeteer'
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+// utill
+var jstypestr = (function (global) {
+  var cache = {};
+  return function (obj) {
+    var key;
+    return obj === null ? 'null' // null
+    : obj === global ? 'global' // window in browser or global in nodejs
+    : (key = typeof obj) !== 'object' ? key // basic: string, boolean, number, undefined, function
+    : obj.nodeType ? 'object' // DOM element
+    : cache[key = ({}).toString.call(obj)] // cached. date, regexp, error, object, array, math
+    || (cache[key] = key.slice(8, -1).toLowerCase()); // get XXXX from [object XXXX], and cache it
+  };
+}(this));
+
+function jsobjstr(o){return JSON.stringify(o, null, 4)}
+function jslogtype(o){console.log(jstypestr(o))}
+function jslogobj(o){console.log(jsobjstr(o))}
+function jsjoinkey_field(o,j=1){
+  var r='';for(var i in o){r+=((j>0)?i+' ':'')+o[i]+'\n'}return r
+}
+function jsT2DA(a){
+  return a[0].map(function(_,c){
+    return a.map(function(r){return r[c]})})
+}
+function jsCSVr(a){return `"${a.join('","')}"`}
+function jsACSV(a){return a.map(jsCSVr).join('\n')}
+function jsPEXT(c=undefined){process.exit(c)}
+function jsrn(n=12){return Math.round(Math.random()*(10**n))}
+export const u = { 
+  jstypestr,
+  jsobjstr,
+  jslogtype,
+  jslogobj,
+  jsjoinkey_field,
+  jsT2DA,
+  jsCSVr,
+  jsACSV,
+  jsPEXT,
+  jsrn,
+}
+
+
+// default variables
+const mm = 2
+const jj = 1
+const ww = 400
+const fs = 14
+const ff = "Helvetica"
+const bnn = 'King James Version (1769)'
+const dbn = 'main.db'
+const dbd = './db/'
+const icn = 'title_short'
+const osr = 'o2'
+const oky = 'oe'
+const iin = 'book_info'
+
+export const k = { 
+  q:{
+    m: mm,
+    j: jj,
+    w: ww,
+    s: fs,
+    y: ff,
+    bn: bnn,
+    dbnn: dbn,
+  },
+  ddir: dbd,
+  ic: icn,
+  osrt: osr,
+  okey: oky,
+  ii: iin,
+}
+
+// custom
+export async function kjvhtml_a(tt, m = k.m) {
+  // m = 1, display itallic from kjv
+  tt = (m > 0) ? tt.replace(/<FI>(.*?)<Fi>/g, "<i>$1</i>") : tt
+  // m = 2, show red texts
+  tt = (m > 1) ? tt.replace(/<FR>(.*?)<Fr>/g, "<font color='red'>$1</font>") : tt
+  // m = 3, show strong's number as superscript
+  const href = 'https://biblehub.com'
+  if (m > 2) {
+    tt = tt.replace(/<WH(.*?)>/g, `<sup><a href='${href}/hebrew/$1.htm'>H$1</a></sup>`)
+    tt = tt.replace(/<WG(.*?)>/g, `<sup><a href='${href}/greek/$1.htm'>G$1</a></sup>`)
+  }
+  // remove all other tags
+  tt = tt.replace(/<RF>.*?<Rf>/g, '')
+  tt = tt.replace(/(<\/?(?:font|i|sup|a)[^>]*>)|<[^>]+>/ig, '$1')
+  // console.log(tt)
+  return tt
+}
+
+export function kjvhtml (tt, m=k.m) {
+  // m = 1, display itallic from kjv
+  tt = (m > 0) ? tt.replace(/<FI>(.*?)<Fi>/g, "<i>$1</i>") : tt
+  // m = 2, show red texts
+  tt = (m > 1) ? tt.replace(/<FR>(.*?)<Fr>/g, "<font color='red'>$1</font>") : tt
+  // m = 3, show strong's number as superscript
+  const href='https://biblehub.com'
+  if (m > 2) {
+    tt = tt.replace(/<WH(.*?)>/g, `<sup><a href='${href}/hebrew/$1.htm'>H$1</a></sup>`)
+    tt = tt.replace(/<WG(.*?)>/g, `<sup><a href='${href}/greek/$1.htm'>G$1</a></sup>`)
+  }
+  // remove all other tags
+  tt = tt.replace(/<RF>.*?<Rf>/g,'')
+  tt = tt.replace(/(<\/?(?:font|i|sup|a)[^>]*>)|<[^>]+>/ig, '$1')
+  // console.log(tt)
+  return tt
+}
+
+export function b3(dbnn){return new sqlt3(dbnn)}
+
+export function q_(db,q){return db.prepare(q)}
+
+export function qa(db,q){return q_(db,q).all()}
+
+export function b3q_(dbnn,q){return q_(b3(dbnn),q)}
+
+export function b3qa(dbnn,q){return qa(b3(dbnn),q)}
+
+export function bibleColumns(db){var bn = [] 
+  qa(db,`PRAGMA table_info(t)`).forEach(async(e)=>{
+    var n=e.name;
+    if(n!='vid'&&n!='b'&&n!='c'&&n!='v'){
+      bn.push({n:n})
+    }
+  });return bn
+}
+
+export function biblebn(dbnn,b){
+  return b3qa(dbnn,`SELECT ${k.ic} FROM ${k.ii} WHERE ${k.okey} IS ${b}`)
+}
+
+export function bibledbqs(b,c,a,o,bn=k.q.bn){
+  return `
+  SELECT ${bn} FROM t 
+  WHERE b is ${b}
+  AND c is ${c}
+  AND v BETWEEN ${a} AND ${o}`
+}
+
+export function bibledbquery(b,c,a,o,bn=k.q.bn,dbnn=k.q.dbnn){
+  return b3qa(dbnn,bibledbqs(b,c,a,o,bn))
+}
+
+export async function bibledbA(b,c,a,o,bn=k.q.bn,dbnn=k.q.dbnn,albn=undefined)
+{return bb.map(bibledbquery(b,c,a,o,(albn)?albn:bn,dbnn),async(e)=>{return e[bn]})}
+
+export function bibledbobj(b,c,a,o,bn=k.q.bn,dbnn=k.q.dbnn){
+  const ro = bibledbquery(b,c,a,o,bn,dbnn);const re={};
+  ro.forEach(function(r,i){re[a+i]=r[bn]});return re
+}
+
+export function htmlfromdb(b,c,a,o,m=k.q.m,j=k.q.j,bn=k.q.bn,dbnn=k.q.dbnn){
+  return kjvhtml(jsjoinkey_field(bibledbobj(b,c,a,o,bn,dbnn),j),m)
+}
+
+export function tafromdb(b,c,a,o,m=k.q.m,j=k.q.j,bn=k.q.bn,dbnn=k.q.dbnn) {
+  const r=htmlfromdb(b,c,a,o,m,j,bn,dbnn).split('\n');r.pop();return r
+}
+
+export async function qiq_(q){
+  var[qi,b,c,a,o,m,j,w,s,y,bn,dbnn]= 
+  ['i','b','c','a','o','m','j','w','s','y','bn','dbnn']
+  var[qi,b,c,a,o,m,j,w,s,y,bn,dbnn]= 
+  [q[qi],q[b],q[c],q[a],q[o],q[m],q[j],q[w],q[s],q[y],q[bn],q[dbnn]]
+  dbnn=k.ddir+dbnn;
+  var p=`${biblebn(dbnn,b)[0][k.ic]} ${c}:${a}-${o}`
+  var albn=`"${bn}"`; var wi=w-20; var _p=jsrn(8)+'_'+p; 
+  var q=(await bibledbA(b,c,a,o,bn,dbnn,albn))
+  q=await bb.map(q,async(t,i)=>{
+    return{w:wi,s:s,y:y,p:_p,n:+a+i,t:t,m:m,j:j,qi:qi}})
+  q.unshift({w:w,s:s,y:y,n:_p,t:p,qi:qi});return q
+}
+
+export async function qiqo(q){
+  var [q,u]=[(await bb.map(q,qiq_)),[]]
+  await bb.each(q,async(i)=>{u.push(i[0])})
+  q=q.flat();return [u,q]
+}
+  
+export function pppe(e){
+  console.error(e)
+  process.exit(0)
+}  
+
+export function uc(o){
+  var s = document.getElementById('s')
+  var c = `
+  .qi${o.qi}{
+    width: ${o.w-20-4}pt;
+    font-size: ${o.s}pt;
+    font-family: ${o.y};
+  }  
+  `
+  s.appendChild(document.createTextNode(c))  
+}  
+
+export async function ucss(p,tu){
+  await bb.each(tu,async(o)=>{
+    await p.evaluate(uc,o)})
+}
+
+export function htch(qi,t){
+  e=document.createElement('div');
+  e.className='qi'+qi;e.innerHTML=t
+  document.body.append(e)
+  return e.clientHeight
+}  
+
+export async function getTNHC(p,qi,t){
+  return await p.evaluate(htch,qi,t)
+}
+
+export async function qo2l(qo){var [tu,tt]=qo
+  try {    
+    console.log(`launching puppeteer...`)
+    const b = await pppt.launch()
+    const p = await b.newPage();
+    await p.goto(`${__dirname}/ht.html`)
+    await ucss(p,tu)
+
+    const ce=Math.ceil
+    tt = await bb.map(tt,async(t)=>{
+      var [w,s,y,p_,n,t_,m,j,qi] = 
+      ['w','s','y','p','n','t','m','j','qi']
+      var [w,s,y,p_,n,t_,m,j,qi] = 
+      [t[w],t[s],t[y],t[p_],t[n],t[t_],t[m],t[j],t[qi]]
+      
+      // joining verse number
+      if(j){t_=n+' '+t_};var l=[]
+
+      if(p_){
+        const bt=await kjvhtml_a(t_,0)
+        const ht=await kjvhtml_a(t_,m)
+        var h=(await getTNHC(p,qi,ht))*3/4
+        // height < 40,       are rounded to the nearest ceiling of 10,
+        // 40 < height <= 45, are rounded to 50,
+        // height >45,        are rounded to the nearest ceiling of 70,90,110, forward and onward.
+        h=(h<40)?ce(h/10)*10:(h<45)?50:ce(h/20)*20+10
+        var a=(h<50)?12:(h<120)?8:4   // arcSize 0-50:12;50-120:8;120<h:4
+        l=[h,a,0,10,w,s,y,p_,n+'_'+Math.round(Math.random()*1e12),bt,ht]
+      }else{l=[0,0,0,0,w,'','','',n,t_,t_]}
+      return l
+    })
+    console.log(`closing puppeteer...`)
+    await b.close()
+
+    // update heights and positions
+    var [lpos,hpos,h_id]=[0,0,0];
+    tt.forEach((t,i)=>{
+      // update top
+      if(!+t[0]){
+        // update swimlane height
+        if(i>0){tt[h_id][0]=hpos;h_id=i}
+        hpos=30;
+      }else{t[2]+=hpos;hpos+=+t[0]+10}      
+      // update left
+      if(!+t[3]){t[3]+=lpos;lpos+=+t[4]+10}
+      // update swimlane height on last row
+      if(i==tt.length-1){tt[h_id][0]=hpos}
+    })
+    
+  }catch(e){pppe(e)}return tt
+}
+
+export const drawiocsvheader = `## Double # are used for comment.
+# style: fontSize=%fontSize%;fontFamily=%fontFamily%;whiteSpace=wrap;align=left;html=1;rounded=1;arcSize=%arcSize%;
+# width: @width
+# height: @height
+# label: %label%
+# identity: partName
+# parent: parent
+# parentstyle: swimlane;startSize=20;
+# top: top
+# left: left
+height,arcSize,top,left,width,fontSize,fontFamily,parent,partName,text,label
+`
+
+export async function qi2l(qi){
+  var qo=await qiqo(qi)
+  // console.log(qo);
+  var l_=await qo2l(qo)
+  // console.log(l_);
+  var l_=drawiocsvheader+jsACSV(l_)
+  return l_
+}
+
+export default { 
+  // js export default multiple functions: https://stackoverflow.com/a/33179024.
+  k,
+  u,
+  kjvhtml,
+  kjvhtml_a,
+  b3,
+  b3q_,
+  b3qa,
+  bibleColumns,
+  biblebn,
+  bibledbqs,
+  bibledbquery,
+  bibledbA,
+  bibledbobj,
+  htmlfromdb,
+  tafromdb,
+  qiq_,
+  qiqo,
+  pppe,
+  uc,
+  ucss,
+  htch,
+  getTNHC,
+  qo2l,
+  drawiocsvheader,
+  qi2l,
+}
---
+++ server.js
@@ -0,0 +1,300 @@
+import ep from "express";
+import hb from "express-handlebars";
+
+import cl from "./bibledrawiolib.js";
+
+import sess from 'express-session';
+
+import { readdirSync } from "fs";
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+// jsPEXT()
+// server ocde
+const web_server = ep()
+var port = 8000
+
+web_server.engine('handlebars', hb())
+web_server.set('view engine', 'handlebars')
+// body parser
+web_server.use(ep.urlencoded())
+web_server.use(sess({
+  secret:'secret-key',
+  resave: false,
+  saveUninitialized: false,
+}))
+
+const k_ = cl.k
+const kq = k_.q
+const u_ = cl.u
+
+var [ic,osrt,okey,ii] = 
+['ic','osrt','okey','ii']
+var [ic,osrt,okey,ii] = 
+[k_[ic],k_[osrt],k_[okey],k_[ii]]
+
+var ia = {
+  b:0,c:0,a:0,o:0,
+  ...kq,
+}
+
+var db__ = await Promise.all(readdirSync(k_.ddir).map(async(e)=>{return{n:e}}))
+
+var ddb3=cl.b3(k_.ddir+kq.dbnn)
+var b_ = ddb3.prepare(`SELECT ${okey}, ${ic} FROM ${ii} ORDER BY ${osrt}`).all()
+var la = {l_:{c:[{}],a:[{}],o:[{}],bn:cl.bibleColumns(ddb3)}}
+ddb3.close()
+
+// cl.u.jslogobj(la)
+// jsPEXT()
+
+// reload l_
+async function rel_(i_) {
+  return await Promise.all(i_.map(async(i)=>{
+    return {i:i.i,l_:(i.l_)?i.l_:la.l_} //
+  }))
+}
+
+// set properties of l_
+async function l_l_(f,l,i) {
+  var db=cl.b3(k_.ddir+i.dbnn)
+  // console.log(i.dbnn);
+  
+  // populate l.bn
+  if (f=='dbnn'){l.bn=l_bn(db)}
+
+  // if the first character is b, then load c_
+  if(f=='b'||i.b){
+    l.c=db.prepare(`SELECT DISTINCT c FROM t WHERE b is `+i.b).all()}
+    if(+i.c>l.c.length){i.c=1;i.a=1;i.o=1}
+    
+  // if the first character is c, then load a_
+  if(f=='c'||i.b&&i.c){
+    l.a=db.prepare(`SELECT v FROM t WHERE b is ${i.b} AND c is ${i.c}`).all()}
+  if(+i.a>l.a.length){i.a=1;i.o=1}
+  if(+i.o>l.a.length){i.o=1}
+  
+  // if the first character is a, then load o_=a.slice(...)
+  if(f=='a'||i.b&&i.c&&i.a){l.o=l.a.slice(i.a-1)}
+  if(+i.a>+i.o){i.o=i.a};
+
+  db.close()
+}    
+
+function servererrorhandling(qo,an,er){
+  console.error(er);
+  qo.session.m = undefined
+  qo.session.l_ = undefined
+  an.redirect('/')
+}
+
+// get request api
+web_server.get('/',async(qo,an)=>{
+  try {
+    var s=qo.session
+    var m=s.m;if(m){m=m[0]}
+    
+    // parse or initialise i_ 
+    var qu=qo.query
+    var l=Object.entries(qu).length
+    var i_=(l)?JSON.parse(qu.i_):[{i:0,...ia}]
+    // console.log(i_);
+    
+    // initialise or parse l_
+    if(!(s.l_)){s.l_=await rel_(i_)};var l_=s.l_
+    // cl.u.jslogobj(l_);
+    
+    // set l_ for html form
+    if(m){
+      if(m!='change'){
+        // console.log(m);
+        var [f,i]=m.split('_');l=l_[i].l_;i=i_[i];await l_l_(f,l,i)  
+      }
+    }else{
+      await l_.forEach(async(l,i)=>{l=l.l_;i=i_[i];await l_l_('',l,i)})
+    }
+
+    // result
+    var r='';if(qu.r){r=await cl.qi2l(i_)}
+    
+    i_=await Promise.all(i_.map(async(e,i)=>{return{...e,l_:l_[i].l_}}))
+
+    var i_st=JSON.stringify(i_)
+    an.render('home',{db__,b_,i_,i_st,r})
+
+  }catch(er){servererrorhandling(qo,an,er)}
+  
+})
+
+// post request to make changes to existing i_.
+web_server.post('/', async (qo,an) => {
+  try {
+    // get i_ and m token
+    var b=qo.body
+    var i_=JSON.parse(b.i_)
+    var m=Object.entries(b)[1]
+    
+    // change i_
+    if(m[0]=='change'){      
+      if(m[1]=='more'){i_.push({i:i_.length,...ia})}
+      if(m[1]=='less'){i_.pop()}    
+      qo.session.l_=await rel_(i_)
+    }else{
+      var [f,i]=m[0].split('_')
+      i_[i][f]=m[1]
+    }
+    
+    // set m, separate l_, get api call.
+    qo.session.m=m
+    i_.forEach(async(i)=>{delete i.l_})
+  
+    var rurl='/?i_='+JSON.stringify(i_)
+    if(m[1]=='submit'){rurl+='&r=1'}
+  
+    // console.log('redirection');
+    an.redirect(rurl)
+
+  }catch(er){servererrorhandling(qo,an,er)}
+
+})
+
+web_server.get('/home.js', async (qo,an)=>{
+  an.sendFile(__dirname+'/home.js')
+})
+
+web_server.listen(port,()=>{
+  console.log(`listening on localhost:${port}`);
+})
+
+// cl.u.jslogobj(i0)
+// cl.u.jslogobj(JSON.parse(i0.st))
+// cl.u.jslogtype(i0)
+// cl.u.jslogtype(i0.st)
+// console.log(i0.st);
+// console.log(i0);
+// cl.u.jslogtype(JSON.parse(i0.st))
+// process.exit()
+
+// function iadd(i_,f=0){i_.push({i:i_.length,...(f>0)?fa:ia})}
+// async function i_2u(i_,n_=1){
+//   var s = (await bb.map(i_, async (e) => {    
+  //     var l = (await bb.map(Object.entries(e), async (_) => {
+    //       // console.log(_[0]);
+    //       return (_[0].slice(-1) != '_')?_[1]:''
+    //     })).join('-').replace((n_)?/(-)\1+/g:'','')
+//     // console.log(l);
+//     return l
+//   })).join('--')
+//   // console.log(s);
+//   // process.exit()
+//   return s
+// }
+
+// async function qoi_(qo,u=undefined) {
+  //   console.log(u);
+  //   return await bb.map((u)?u:qo.params.list.split('--'), async(e)=>{
+    //     e = e.split('-') 
+    //     // console.log(e);
+    //     if (e.length == 12) {
+      //       return  {
+//         i:e[0],
+//         dbnn:e[1],bn:e[2],
+//         b:e[3],c:e[4],a:e[5],o:e[6],
+//         m:e[7],j:e[8],
+//         w:e[9],s:e[10],y:e[11],
+//       }
+//     }else{
+  //       console.log('error: list shorter than 12 elements');
+  //       // need error handling on api entries other than 12 elements      
+  //     }});
+// }
+
+// web_server.get('/:list/:change', async (qo,an)=>{
+  //   var p = qo.params
+  //   var [u,mm] = [p.list.split('--'),p.change.split('-')]
+  //   // console.log(i_);
+  //   // console.log(mm);
+  
+  //   if (mm.length==2) {
+    //     if (mm[0]=='change'){      
+      //       if (mm[1]=='more') {
+        //         u=[...u,await i_2u([{i:u.length,...ia}])] //
+        //       }
+        //       if(mm[1]=='less'){u.pop()}
+        //     }else{
+          //       var [f,i]=mm[0].split('_')
+          //       var ii = await qoi_('',[u[i]])  //
+          //       ii[0][f] = mm[1]
+          //       u[i] = await i_2u(ii) //
+//     }    
+//   }
+//   u=u.join('--')
+  
+//   an.redirect('/'+u)
+//   // an.send('debug mode (debugging)')
+//   // an.send(sl)
+// })
+
+// web_server.get('/:list/', async (qo,an)=>{
+//   // render page according to api
+//   // console.log(qo.params.list);
+  
+//   // console.log(i_);
+//   // console.log(i_.length);
+//   // // (e.g. i_[0]['b']=test['b0'])
+//   // ii[f] = test[f0]
+
+
+//   var i_ = await bb.map((await qoi_(qo)), async(ii)=>{  //
+//     // cl.u.jslogobj(ii)
+//     ii.c_=[{}];ii.a_=[{}];ii.o_=[{}];
+//     // if the first character is b, then load c_,
+//     if(ii.b){
+//       ii.c_=await db.prepare(`SELECT DISTINCT c FROM t WHERE b is `+ii.b).all()}
+//     if(+ii.c>ii.c_.length){ii.c=1;ii.a=1;ii.o=1}
+    
+//     // if the first character is c, then load a_
+//     if(ii.b&&ii.c){
+//       ii.a_=await db.prepare(`SELECT v FROM t WHERE b is ${ii.b} AND c is ${ii.c}`).all()}
+//     if(+ii.a>ii.a_.length){ii.a=1;ii.o=1}
+//     if(+ii.o>ii.a_.length){ii.o=1}
+    
+//     // if the first character is a, then load o_=a.slice(...)
+//     if(ii.b&&ii.c&&ii.a){ii.o_=ii.a_.slice(ii.a-1)}
+//     if(+ii.a>+ii.o){ii.o=ii.a};
+//     // ii = {...ii,...c_,...a_,...o_}
+//     // cl.u.jslogobj(ii)
+//     return ii
+//   })
+  
+//   // cl.u.jslogobj(i_)
+  
+//   var i_st = JSON.stringify(i_)
+//   an.render('home', { b_,i_,i_st })
+//   // process.exit()
+
+//   // cl.u.jslogobj(i_);
+  
+//   // an.send('landed on rendering api')
+// })
+
+// web_server.post('/b', async (qo,an) => {
+//   b = qo.body.b;an.redirect('/')
+// })
+
+// web_server.post('/c', async (qo,an) => {
+//   c = qo.body.c;an.redirect('/')
+// })
+
+// web_server.post('/a', async (qo,an) => {
+//   a = qo.body.a;an.redirect('/')
+// })
+
+// web_server.post('/o', async (qo,an) => {
+//   o = qo.body.o;an.redirect('/')
+// })
+
