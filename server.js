f803c732e14e4f813fee9ad7b691af37b2bdcddd
Kindnessfruit <106155333+Kindnessfruit@users.noreply.github.com>
2025-07-01 18:06:02 +0800

    https

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-import http from "http";
+import https from "https";
 
 
 const host = ep()
